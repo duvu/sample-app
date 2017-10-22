99ea93ba587e4c634eee540a8a3e2ffe9eadb994
beou <hoaivubk@gmail.com>
2017-10-23 03:00:41 +0700

    update account

--- src/app/main/administration/account/account.component.ts
+++ src/app/main/administration/account/account.component.ts
@@ -33,24 +33,24 @@ export class AccountComponent implements OnInit {
     displayedColumns = ['id', 'accountId', 'firstName', 'lastName', 'organizationName', 'notes', 'createdBy', 'createdOn', 'actions'];
 
     columns = {
-        id: {selected: false, order: 0},
-        accountId: {selected: false, order: 1},
-        firstName: {selected: false, order: 2},
-        lastName: {selected: false, order: 3},
-        status: {selected: false, order: 4},
-        organizationId: {selected: false, order: 5},
-        organizationName: {selected: false, order: 6},
-        phoneNumber: {selected: false, order: 7},
-        photoUrl: {selected: false, order: 8},
-        emailAddress: {selected: false, order: 9},
-        addressLine1: {selected: false, order: 10},
-        addressLine2: {selected: false, order: 11},
-        notes: {selected: false, order: 12},
-        createdBy: {selected: false, order: 13},
-        createdOn: {selected: false, order: 14},
-        updatedBy: {selected: false, order: 15},
-        updatedOn: {selected: false, order: 16},
-        actions: {selected: false, order: 17}
+        id:                 {selected: false, order: 0},
+        accountId:          {selected: false, order: 1},
+        firstName:          {selected: false, order: 2},
+        lastName:           {selected: false, order: 3},
+        status:             {selected: false, order: 4},
+        organizationId:     {selected: false, order: 5},
+        organizationName:   {selected: false, order: 6},
+        phoneNumber:        {selected: false, order: 7},
+        photoUrl:           {selected: false, order: 8},
+        emailAddress:       {selected: false, order: 9},
+        addressLine1:       {selected: false, order: 10},
+        addressLine2:       {selected: false, order: 11},
+        notes:              {selected: false, order: 12},
+        createdBy:          {selected: false, order: 13},
+        createdOn:          {selected: false, order: 14},
+        updatedBy:          {selected: false, order: 15},
+        updatedOn:          {selected: false, order: 16},
+        actions:            {selected: false, order: 17}
     };
 
     constructor(private dialog: MatDialog,
@@ -132,17 +132,35 @@ export class AccountComponent implements OnInit {
 
         dialogRef.afterClosed().subscribe(result => {
             if (result) {
-                this.createObject(result);
+                this.create(result);
             }
         });
     }
 
-    createObject(account: Account): void {
+    create(account: Account): void {
         this.service.create(account).subscribe(
             data => {
                 this.dataChange.next(data.id);
             }
         );
+    }
+
+    openDialogEditing(data: Account): void {
+        const dialogRef = this.dialog.open(AddEditAccountComponent, {
+            // width: '600px',
+            disableClose: true,
+            data: data
+        });
+
+        dialogRef.afterClosed().subscribe(result => {
+            if (result) {
+                this.update(result);
+            }
+        });
+    }
+
+    update(account: Account): void {
+
     }
 
     openDialogConfirmDelete(account: Account): void {
