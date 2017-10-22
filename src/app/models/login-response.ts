8bbdbb439a72e0e197f9411c59e0373a4c20dd0b
beou <hoaivubk@gmail.com>
2017-10-22 21:23:41 +0700

    deleted account

--- src/app/models/login-response.ts
+++ src/app/models/login-response.ts
@@ -1,12 +1,93 @@
 export class LoginResponse {
-    access_token: string;
-    accountId: number;
-    accountName: string;
-    authorities: string[];
-    expires_in: number;
-    jti: string;
-    organizationId: number;
-    organizationName: string;
-    scope: string;
-    token_type: string;
+    private _access_token: string;
+    private _accountId: number;
+    private _accountName: string;
+    private _authorities: string[];
+    private _expires_in: number;
+    private _jti: string;
+    private _organizationId: number;
+    private _organizationName: string;
+    private _scope: string;
+    private _token_type: string;
+
+
+    get access_token(): string {
+        return this._access_token;
+    }
+
+    set access_token(value: string) {
+        this._access_token = value;
+    }
+
+    get accountId(): number {
+        return this._accountId;
+    }
+
+    set accountId(value: number) {
+        this._accountId = value;
+    }
+
+    get accountName(): string {
+        return this._accountName;
+    }
+
+    set accountName(value: string) {
+        this._accountName = value;
+    }
+
+    get authorities(): string[] {
+        return this._authorities;
+    }
+
+    set authorities(value: string[]) {
+        this._authorities = value;
+    }
+
+    get expires_in(): number {
+        return this._expires_in;
+    }
+
+    set expires_in(value: number) {
+        this._expires_in = value;
+    }
+
+    get jti(): string {
+        return this._jti;
+    }
+
+    set jti(value: string) {
+        this._jti = value;
+    }
+
+    get organizationId(): number {
+        return this._organizationId;
+    }
+
+    set organizationId(value: number) {
+        this._organizationId = value;
+    }
+
+    get organizationName(): string {
+        return this._organizationName;
+    }
+
+    set organizationName(value: string) {
+        this._organizationName = value;
+    }
+
+    get scope(): string {
+        return this._scope;
+    }
+
+    set scope(value: string) {
+        this._scope = value;
+    }
+
+    get token_type(): string {
+        return this._token_type;
+    }
+
+    set token_type(value: string) {
+        this._token_type = value;
+    }
 }
--- src/app/services/auth-interceptor.ts
+++ src/app/services/auth-interceptor.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
+import {AppService} from "./app.service";
+import {Router} from "@angular/router";
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        console.log('URL', req.url);
+    constructor(private app: AppService, private router: Router) {
+    }
 
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         if (req.url.startsWith('/oauth')) {
             const changeReg = req.clone();
             return next.handle(changeReg);
         } else {
-                const currentUser = JSON.parse(localStorage.getItem('currentUser'));
-                const token1 = currentUser && currentUser.access_token;
-                const token = 'Bearer ' + token1;
+                const token = this.app.getToken();
                 const changeReg = req.clone({
                     headers: req.headers.set('Content-Type', 'application/json')
                         .set('Accept', 'application/json')
