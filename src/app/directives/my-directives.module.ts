2624de4bfd305a0e534d41b7180db7b1372596b6
beou <hoaivubk@gmail.com>
2017-12-28 15:09:20 +0700

    added directives module

--- src/app/directives/my-directives.module.ts
+++ src/app/directives/my-directives.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IsAdminDirective } from './is-admin.directive';
+import { ShowIfRoleDirective } from './show-if-role.directive';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [IsAdminDirective],
-  exports: [IsAdminDirective]
+  declarations: [
+      IsAdminDirective,
+      ShowIfRoleDirective
+  ],
+  exports: [
+      IsAdminDirective,
+      ShowIfRoleDirective
+  ]
 })
 export class MyDirectivesModule { }
