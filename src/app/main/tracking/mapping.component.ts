405f8e59899fc1affa97a2772dfe0944ed0a8a32
beou <hoaivubk@gmail.com>
2018-01-08 05:31:21 +0700

    changed device-projection

--- src/app/main/tracking/mapping.component.ts
+++ src/app/main/tracking/mapping.component.ts
@@ -148,7 +148,7 @@ export class MappingComponent implements OnInit, OnDestroy, AfterViewInit {
         let popup = L.popup();
         let htmlPopup = '';
 
-        let txtDate = this._datePipe.transform(event.timestamp * 1000, 'MMM dd, yyyy hh:mm:ss');
+        let txtDate = this._datePipe.transform(event.timestamp, 'MMM dd, yyyy hh:mm:ss');
         htmlPopup += '<table>';
         htmlPopup += '<tr>'; htmlPopup += '<td class="popup-title">';htmlPopup += 'DeviceID:'; htmlPopup += '</td>';htmlPopup += '<td>';htmlPopup += event.deviceId;htmlPopup += '</td>';htmlPopup += '</tr>';
         htmlPopup += '<tr>'; htmlPopup += '<td class="popup-title">';htmlPopup += 'Time:'; htmlPopup += '</td>';htmlPopup += '<td>';htmlPopup += txtDate;htmlPopup += '</td>';htmlPopup += '</tr>';
