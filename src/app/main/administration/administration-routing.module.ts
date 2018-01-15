83eb2ee261853d6a3b84f4192631d8540b094365
beou <hoaivubk@gmail.com>
2018-01-16 00:24:20 +0700

    added d3js

--- src/app/main/administration/administration-routing.module.ts
+++ src/app/main/administration/administration-routing.module.ts
@@ -6,6 +6,7 @@ import {AccountComponent} from "./account/account.component";
 import {DeviceComponent} from "./device/device.component";
 import { CompanyComponent } from 'app/main/administration/company/organization.component';
 import { PrivilegeComponent } from 'app/main/administration/privilege/privilege.component';
+import { DcsComponent } from 'app/main/administration/dcs/dcs.component';
 
 const routes: Routes = [
     {
@@ -15,7 +16,8 @@ const routes: Routes = [
             { path: '_account', component: AccountComponent, canActivate: [AuthGuard] },
             { path: '_company',   component: CompanyComponent,    canActivate: [AuthGuard] },
             { path: '_device',   component: DeviceComponent,    canActivate: [AuthGuard] },
-            { path: '_privilege',   component: PrivilegeComponent,    canActivate: [AuthGuard] }
+            { path: '_privilege',   component: PrivilegeComponent,    canActivate: [AuthGuard] },
+            { path: '_dcs',   component: DcsComponent,    canActivate: [AuthGuard] }
         ]
     }
 ];
