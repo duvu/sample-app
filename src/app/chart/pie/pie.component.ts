81230443e7220f23f33c630b6dabb6c433c8b1d5
beou <hoaivubk@gmail.com>
2018-01-17 01:31:08 +0700

    added pie-chart

---
+++ src/app/chart/pie/pie.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pie-chart',
+  templateUrl: './pie.component.html',
+  styleUrls: ['./pie.component.scss']
+})
+export class PieComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
