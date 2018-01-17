f94ffde862c8d2526f401a34538eb16bc40d0bfb
beou <hoaivubk@gmail.com>
2018-01-17 16:39:21 +0700

    add history page

--- src/app/main/tracking/mapping.module.ts
+++ src/app/main/tracking/mapping.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from "@angular/forms";
-import { MappingRoutingModule } from './mapping-routing.module';
-import { MappingComponent } from './mapping.component';
+import { MappingRoutingModule } from 'app/main/tracking/mapping-routing.module';
+import { MappingComponent } from 'app/main/tracking/live/mapping.component';
 import {CommonModule, DatePipe} from "@angular/common";
 import {DeviceService} from 'app/shared/services/device.service';
 import {EventService} from 'app/shared/services/event.service';
 import { MaterialShared } from 'app/shared/material-shared';
+import { HistoryComponent } from './history/history.component';
 
 
 @NgModule({
@@ -20,6 +21,6 @@ import { MaterialShared } from 'app/shared/material-shared';
         EventService,
         DatePipe
     ],
-    declarations: [MappingComponent]
+    declarations: [MappingComponent, HistoryComponent]
 })
 export class MappingModule { }
--- src/app/shared/services/account.service.ts
+++ src/app/shared/services/account.service.ts
@@ -4,6 +4,7 @@ import {Router} from '@angular/router';
 import {HttpClient} from '@angular/common/http';
 import { Base2Service } from 'app/shared/services/base2.service';
 import { AccountRequest } from 'app/shared/models/request/request-account';
+import { Observable } from 'rxjs/Rx';
 
 export const ACCOUNT_API_URL = '/api/account';
 
@@ -11,5 +12,10 @@ export const ACCOUNT_API_URL = '/api/account';
 export class AccountService extends Base2Service<AccountRequest, Account> {
     constructor(private http: HttpClient, private router: Router) {
         super(http, router, ACCOUNT_API_URL);
+    }
+
+    getAllStatus(): Observable<string[]> {
+        const url = ACCOUNT_API_URL + '/status';
+        return this.http.get<string[]>(url);
     }
 }
