a465b140c4022ac393d434e5c42805d7249f343d
beou <hoaivubk@gmail.com>
2018-01-18 16:26:31 +0700

    add history page

--- src/app/main/tracking/history/history.component.ts
+++ src/app/main/tracking/history/history.component.ts
@@ -3,13 +3,14 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 import 'rxjs/add/operator/switchMap';
 import * as L from 'leaflet';
 import 'leaflet-polylinedecorator';
+import 'leaflet-easybutton';
+
 //import 'leaflet.markercluster';
 import { EventService } from 'app/shared/services/event.service';
 import { EventData } from 'app/shared/models/event-data';
 
 import * as _ from 'lodash';
-import { LatLngBounds } from 'leaflet';
-import { LatLng } from 'leaflet';
+import { LatLng, Polyline } from 'leaflet';
 import { MatTableDataSource } from '@angular/material';
 
 
@@ -27,10 +28,15 @@ export class HistoryComponent implements OnInit, AfterViewInit {
     private id: string;
     private map: L.Map;
     private historyEvents: EventData[];
+    private polyline: Polyline;
+    private decor: any;
+    private timeFrom: number;
+    private  timeTo: number;
+
     private customDefault: L.Icon;
 
     dataSource: MatTableDataSource<EventData> | null;
-    displayedColumns = ['id', 'deviceId', 'deviceName', 'latitude', 'longitude', 'altitude', 'heading', 'speedKPH', 'address'];
+    displayedColumns = ['id', 'deviceId', 'deviceName', 'latitude', 'longitude', 'altitude', 'heading', 'speedKPH', 'address', 'timestamp'];
 
     constructor(private route: ActivatedRoute,
                 private eventService: EventService) { }
@@ -44,6 +50,8 @@ export class HistoryComponent implements OnInit, AfterViewInit {
         });
 
         this.dataSource = new MatTableDataSource();
+        this.timeFrom = this.timeFrom ? this.timeFrom : 0;
+        this.timeTo = this.timeTo ? this.timeTo : 0;
     }
     ngAfterViewInit(): void {
         console.log('afterView');
@@ -64,11 +72,62 @@ export class HistoryComponent implements OnInit, AfterViewInit {
 
         L.control.scale().addTo(this.map);
         L.control.zoom().setPosition('bottomleft').addTo(this.map);
+
+        let range = [];
+
+        let range1h = L.easyButton('<span>1h</span>', function (button, map) {
+            this.timeTo = (new Date()).getTime();
+            this.timeFrom = this.timeTo - 60 * 60 * 1000;
+            this.loadHistoryEvents();
+        }.bind(this));
+
+        let range2h = L.easyButton('<span>2h</span>', function () {
+            this.timeTo = (new Date()).getTime();
+            this.timeFrom = this.timeTo - 2 * 60 * 60 * 1000;
+            this.loadHistoryEvents();
+        }.bind(this));
+        let range6h = L.easyButton('<span>6h</span>', function () {
+            this.timeTo = (new Date()).getTime();
+            this.timeFrom = this.timeTo - 6 * 60 * 60 * 1000;
+            this.loadHistoryEvents();
+        }.bind(this));
+        let range12h = L.easyButton('<span>12h</span>', function () {
+            this.timeTo = (new Date()).getTime();
+            this.timeFrom = this.timeTo - 12 * 60 * 60 * 1000;
+            this.loadHistoryEvents();
+        }.bind(this));
+
+        let range1d = L.easyButton('<span>1d</span>', function () {
+            this.timeTo = (new Date()).getTime();
+            this.timeFrom = this.timeTo - 24 * 60 * 60 * 1000;
+            this.loadHistoryEvents();
+        }.bind(this));
+        let range3d = L.easyButton('<span>3d</span>', function () {
+            this.timeTo = (new Date()).getTime();
+            this.timeFrom = this.timeTo - 72 * 60 * 60 * 1000;
+            this.loadHistoryEvents();
+        }.bind(this));
+
+        let range1w = L.easyButton('<span>1w</span>', function () {
+            this.timeTo = (new Date()).getTime();
+            this.timeFrom = this.timeTo - 7 * 24* 60 * 60 * 1000;
+            this.loadHistoryEvents();
+        }.bind(this));
+        let range1m = L.easyButton('<span>1m</span>', function () {
+            this.timeTo = (new Date()).getTime();
+            this.timeFrom = this.timeTo - 30 * 24* 60 * 60 * 1000;
+            this.loadHistoryEvents();
+        }.bind(this));
+
+        range.push(range1h, range2h, range6h, range12h, range1d, range3d, range1w, range1m);
+
+        L.easyBar(range).addTo(this.map);
+
         this.loadHistoryEvents();
     }
 
     private loadHistoryEvents(): void {
-        this.eventService.getHistoryEvents(this.id, 0, 0).subscribe(
+        this.eventService.getHistoryEvents(this.id, this.timeFrom, this.timeTo).subscribe(
             data => {
                 //console.log('Data', data);
                 this.historyEvents = data;
@@ -82,18 +141,22 @@ export class HistoryComponent implements OnInit, AfterViewInit {
     }
 
     private processEvents(): void {
-        // _.forEach(this.historyEvents, function (event) {
-        //     L.marker([event.latitude, event.longitude], {icon: this.customDefault}).addTo(this.map);
-        // }.bind(this));
         let latlngs = _.map(this.historyEvents, (event) => {
            return new LatLng(event.latitude, event.longitude);
         });
 
-        console.log('LL', latlngs);
-        let polyline = L.polyline(latlngs, {color: 'red'}).addTo(this.map);
-        this.map.fitBounds(polyline.getBounds());
+        if (this.polyline) {
+            this.map.removeLayer(this.polyline);
+        }
 
-        let decor = L.polylineDecorator(polyline, {
+        if (this.decor) {
+            this.map.removeLayer(this.decor);
+        }
+
+        this.polyline = L.polyline(latlngs, {color: 'red'}).addTo(this.map);
+        this.map.fitBounds(this.polyline.getBounds());
+
+        this.decor = L.polylineDecorator(this.polyline, {
             patterns: [
                 {offset: 25, repeat: 50, symbol: L.Symbol.arrowHead({pixelSize: 15, pathOptions: {fillOpacity: 1, weight: 0}})}
             ]
