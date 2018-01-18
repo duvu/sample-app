ae63316f8d6a43615906ca70f23a860628d4e644
beou <hoaivubk@gmail.com>
2018-01-18 20:30:24 +0700

    refactor code

--- src/app/main/tracking/mapping.module.ts
+++ src/app/main/tracking/mapping.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from "@angular/forms";
 import { MappingRoutingModule } from 'app/main/tracking/mapping-routing.module';
 import { MappingComponent } from 'app/main/tracking/live/mapping.component';
-import {CommonModule, DatePipe} from "@angular/common";
-import {DeviceService} from 'app/shared/services/device.service';
+import { CommonModule, DatePipe} from "@angular/common";
+import { DeviceService} from 'app/shared/services/device.service';
 import {EventService} from 'app/shared/services/event.service';
 import { MaterialShared } from 'app/shared/material-shared';
 import { HistoryComponent } from './history/history.component';
+import { FacilityLinkComponent, LinkPopupService } from 'app/main/tracking/LinkPopupService';
 
 
 @NgModule({
@@ -18,9 +19,18 @@ import { HistoryComponent } from './history/history.component';
     ],
     providers: [
         DeviceService,
+        LinkPopupService,
         EventService,
         DatePipe
     ],
-    declarations: [MappingComponent, HistoryComponent]
+
+    entryComponents: [
+      FacilityLinkComponent
+    ],
+    declarations: [
+        MappingComponent,
+        HistoryComponent,
+        FacilityLinkComponent
+    ]
 })
 export class MappingModule { }
