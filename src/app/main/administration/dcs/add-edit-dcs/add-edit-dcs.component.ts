b00ecbb662dee0d973af2f198b2c40327aed9200
beou <hoaivubk@gmail.com>
2018-02-02 01:30:23 +0700

    added page dcs - refactor

--- src/app/main/administration/dcs/add-edit-dcs/add-edit-dcs.component.ts
+++ src/app/main/administration/dcs/add-edit-dcs/add-edit-dcs.component.ts
@@ -1,8 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { AccountRequest } from 'app/shared/models/request/request-account';
-import { AddEditAccountComponent } from 'app/main/administration/account/add-edit-account/add-edit-account.component';
-import { Account } from 'app/shared/models/account';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { Dcs } from 'app/shared/models/dcs';
 
 @Component({
   selector: 'app-add-edit-dcs',
@@ -12,7 +10,7 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 export class AddEditDcsComponent implements OnInit {
 
   constructor(public dialogRef: MatDialogRef<AddEditDcsComponent>,
-              @Inject(MAT_DIALOG_DATA) public data: Account | any) { }
+              @Inject(MAT_DIALOG_DATA) public data: Dcs | any) { }
 
   ngOnInit() {
   }
