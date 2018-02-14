d9f04a7b6b5e09529c1186ecda47a229d7ed4e74
beou <hoaivubk@gmail.com>
2018-02-14 22:26:45 +0700

    added some component

--- src/app/main/report/device-report/device-report.component.ts
+++ src/app/main/report/device-report/device-report.component.ts
@@ -5,10 +5,10 @@ import { DeviceService } from 'app/shared/services/device.service';
 
 @Component({
     selector: 'app-report',
-    templateUrl: './report.component.html',
-    styleUrls: ['./report.component.scss']
+    templateUrl: './device-report.component.html',
+    styleUrls: ['./device-report.component.scss']
 })
-export class ReportComponent implements OnInit {
+export class DeviceReportComponent implements OnInit {
 
     deviceTableDataSource: MatTableDataSource<DeviceLittle> | null;
     displayedColumns = ['name'];
