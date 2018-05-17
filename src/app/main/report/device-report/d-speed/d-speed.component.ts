ad7ab8ad1a5456730f062072c1e2eb84b703c64d
beou <hoaivubk@gmail.com>
2018-05-17 10:24:15 +0700

    [1.0.0] added 2 component speed-report and parking report

---
+++ src/app/main/report/device-report/d-speed/d-speed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-d-speed',
+  templateUrl: './d-speed.component.html',
+  styleUrls: ['./d-speed.component.scss']
+})
+export class DSpeedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
