0a6f1e2f94a08013643d536df8b106020547bff9
beou <hoaivubk@gmail.com>
2018-08-14 09:58:38 +0700

    [0.0.4] init page preference

--- src/app/main/profile/profile.component.ts
+++ src/app/main/profile/profile.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { ApplicationContext } from 'app/application-context';
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+    selector: 'app-profile',
+    templateUrl: './profile.component.html',
+    styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+    isEditting: boolean;
 
-  constructor() { }
+    accountId: number;
+    accountName: string;
+    companyId: number;
+    companyName: string;
+    access_token: string;
+    token_type: string;
+    jti: string;
+    scope: string;
+    firstPageUrl: string;
+    authorities: Array<string>;
+    redirectURL: string
+    expires_in: number;
 
-  ngOnInit() {
-  }
+
+    constructor(private applicationContext: ApplicationContext) { }
+
+    ngOnInit() {
+        this.isEditting = false;
+
+        this.accountId      = this.applicationContext.accountId;
+        this.accountName    = this.applicationContext.accountName;
+        this.companyId      = this.applicationContext.companyId;
+        this.companyName    = this.applicationContext.organizationName;
+        this.firstPageUrl   = this.applicationContext.firstPageUrl;
+        this.access_token   = this.applicationContext.access_token;
+        this.token_type     = this.applicationContext.token_type;
+        this.jti            = this.applicationContext.jti;
+        this.scope          = this.applicationContext.scope;
+        this.authorities    = this.applicationContext.authorities;
+        this.redirectURL    = this.applicationContext.redirectURL;
+        this.expires_in     = this.applicationContext.expires_in;
+    }
 
 }
